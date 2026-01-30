225e555932c93a178c94c0f04e411c86b1358570
Wahid-Qandil <wahidqandill@gmail.com>
2026-01-30 21:44:26 +0200

    Add navigation for onboarding and login screens, and include preloaded fonts

--- app/src/main/java/org/wahid/foody/MainActivity.java
+++ app/src/main/java/org/wahid/foody/MainActivity.java
@@ -1,8 +1,13 @@
 package org.wahid.foody;
+import android.app.ActionBar;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.SystemBarStyle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
@@ -13,17 +18,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-
+        int color = ContextCompat.getColor(this, R.color.primary);
+        EdgeToEdge.enable(this,
+                SystemBarStyle.dark(
+                        color
+                ),
+                SystemBarStyle.dark(
+                        color
+                )
+        );
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
-
         setContentView(R.layout.activity_main);
+    }
+
+    public void setOnApplyWindowInsets(){
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
     }
 }
