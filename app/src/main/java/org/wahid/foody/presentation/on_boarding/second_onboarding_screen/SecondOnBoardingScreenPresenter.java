04de829ddd7d71f2533d46f74335840a20801fb2
Wahid-Qandil <wahidqandill@gmail.com>
2026-02-02 03:58:26 +0200

    Implement MVP pattern for second onboarding screen and add navigation handling

---
+++ app/src/main/java/org/wahid/foody/presentation/on_boarding/second_onboarding_screen/SecondOnBoardingScreenPresenter.java
@@ -0,0 +1,6 @@
+package org.wahid.foody.presentation.on_boarding.second_onboarding_screen;
+
+public interface SecondOnBoardingScreenPresenter {
+    void onSkip();
+    void onGetStarted();
+}
--- app/src/main/java/org/wahid/foody/presentation/on_boarding/second_onboarding_screen/SecondOnboardingScreenFragment.java
+++ app/src/main/java/org/wahid/foody/presentation/on_boarding/second_onboarding_screen/SecondOnboardingScreenFragment.java
@@ -18,15 +18,17 @@ import org.wahid.foody.databinding.FragmentSecondOnboardingScreenBinding;
 import java.util.Objects;
 
 
-public class SecondOnboardingScreenFragment extends Fragment {
+public class SecondOnboardingScreenFragment extends Fragment implements SecondOnBoardingScreenView {
 
 
-    FragmentSecondOnboardingScreenBinding binding;
+    private FragmentSecondOnboardingScreenBinding binding;
+    private SecondOnBoardingScreenPresenter presenter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         binding = FragmentSecondOnboardingScreenBinding.inflate(getLayoutInflater(),container,false);
+        presenter = new SecondOnBoardingScreenPresenterImpl(binding.getRoot());
         return binding.getRoot();
     }
 
@@ -35,13 +37,8 @@ public class SecondOnboardingScreenFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         binding.secondOnboardingImage.setImageResource(R.drawable.fried_chicken_with_vegetables_herbs_aluminum_skillet);
-
-        binding.getStartedBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(binding.getRoot()).navigate(R.id.action_fragment_second_onboarding_screen_to_fragment_login);
-            }
-        });
+        binding.getStartedBtn.setOnClickListener((v -> {navigateToLogin();}));
+        binding.skipTv.setOnClickListener((v -> {skipOnboardingClicked();}));
     }
 
     @Override
@@ -57,5 +54,15 @@ public class SecondOnboardingScreenFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+    }
+
+    @Override
+    public void navigateToLogin() {
+        presenter.onGetStarted();
+    }
+
+    @Override
+    public void skipOnboardingClicked() {
+        presenter.onSkip();
     }
 }
