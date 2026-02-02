0799c2f80e9698ad26235f42b123046fd80f7014
Wahid-Qandil <wahidqandill@gmail.com>
2026-02-02 21:25:14 +0200

    Add dialog status layout and application class for Firebase authentication

---
+++ app/src/main/java/org/wahid/foody/FoodyApplication.java
@@ -0,0 +1,10 @@
+package org.wahid.foody;
+
+import android.app.Application;
+
+public class FoodyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
