4d06d83e460b5ef6a9f6ed0e76a9f6e59f11ae71
Wahid-Qandil <wahidqandill@gmail.com>
2026-02-07 02:28:39 +0200

    Refactor MVP base interfaces and update Home screen implementation
    - Rename `BasePresenter` to `BaseView` across `LoginView`, `RegisterView`, and `HomeView`
    - Add YouTube player core dependency to `libs.versions.toml`
    - Update `HomeFragment` and `HomePresenterImpl` with required API annotations and presenter initialization logic
    - Refactor `MealRepositoryImpl` and `HomeFragment` to use constructor-injected dependencies
    - Comment out unused test case in `ExampleInstrumentedTest`
    - Clean up unused imports in `RemoteMealDatasource` and `HomePresenterImpl`

--- app/src/main/java/org/wahid/foody/presentation/auth/login/LoginView.java
+++ app/src/main/java/org/wahid/foody/presentation/auth/login/LoginView.java
@@ -1,6 +1,6 @@
 package org.wahid.foody.presentation.auth.login;
 
-import org.wahid.foody.presentation.BasePresenter;
+import org.wahid.foody.presentation.BaseView;
 
-public interface LoginView extends BasePresenter {
+public interface LoginView extends BaseView {
 }
--- app/src/main/java/org/wahid/foody/presentation/auth/register/RegisterView.java
+++ app/src/main/java/org/wahid/foody/presentation/auth/register/RegisterView.java
@@ -2,10 +2,10 @@ package org.wahid.foody.presentation.auth.register;
 
 import androidx.credentials.GetCredentialRequest;
 
-import org.wahid.foody.presentation.BasePresenter;
+import org.wahid.foody.presentation.BaseView;
 
 
-public interface RegisterView extends BasePresenter {
+public interface RegisterView extends BaseView {
     void showGoogleRegisterDialog(GetCredentialRequest request);
     void showFacebookRegisterDialog();
 }
