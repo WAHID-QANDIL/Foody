2e0303cc0a77b8ae9bc9fb2844385f386cf7b774
Wahid-Qandil <wahidqandill@gmail.com>
2026-02-11 01:36:38 +0200

    Refactor ApplicationDependencyRepository to initialize MealRoomDb and update MealRepository instantiation

--- app/src/main/java/org/wahid/foody/utils/ApplicationDependencyRepository.java
+++ app/src/main/java/org/wahid/foody/utils/ApplicationDependencyRepository.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import org.wahid.foody.FoodyApplication;
 import org.wahid.foody.data.MealRepositoryImpl;
+import org.wahid.foody.data.local.database.MealRoomDb;
 import org.wahid.foody.data.remote.meal_service.RemoteMealDatasource;
 import org.wahid.foody.presentation.MealRepository;
 
@@ -13,6 +14,7 @@ public abstract class ApplicationDependencyRepository {
 
     public @Volatile static Application application = FoodyApplication.application;
     public static RemoteMealDatasource remoteMealDatasource = new RemoteMealDatasource();
-    public static MealRepository repository = new MealRepositoryImpl(application,remoteMealDatasource);
+    public static MealRepository repository = new MealRepositoryImpl(remoteMealDatasource);
+    public static MealRoomDb mealRoomDb = MealRoomDb.getInstance(application);
 
 }
